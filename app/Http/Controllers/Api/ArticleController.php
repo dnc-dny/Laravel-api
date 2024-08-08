afbeddc2a23d7dacb6b499648450d49143117a9c
yamazaki <16ic035e@gmail.com>
2024-08-08 10:15:09 +0900

    curlコマンドサーバー接続修正

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Article;
+use App\Models\Article;
 
 class ArticleController extends Controller
 {
